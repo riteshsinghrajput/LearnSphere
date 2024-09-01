510f40c0185d010a192819bca8f4f66ef946a5e3
riteshsinghrajput <93969405+riteshsinghrajput@users.noreply.github.com>
2024-09-01 18:13:57 +0530

    Update axiosInstance.js

--- client/src/helper/axiosInstance.js
+++ client/src/helper/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const BASE_URL = `${import.meta.env.VITE_REACT_APP_SERVER_URL}/api/v1`
-
+console.log(BASE_URL)
 const axiosInstance = axios.create();
 
 axiosInstance.defaults.baseURL = BASE_URL;
